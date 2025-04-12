ec5267610a35e0972617f468f13dc7935fb2d5bf
Jmgty <=>
2025-04-13 00:37:40 +0800

    create car information model for future data

---
+++ lib/models/md-car-infornation.dart
@@ -0,0 +1,27 @@
+class CarInformation {
+  final int id;
+  final String name;
+  final String brand;
+  final String model;
+  final String transmission;
+  final String seating_capacity;
+  final String status;
+  final String rental_price;
+  final String description;
+  final String imageUrl;
+  final String createdAt;
+
+  CarInformation({
+    required this.id,
+    required this.name,
+    required this.brand,
+    required this.model,
+    required this.transmission,
+    required this.seating_capacity,
+    required this.status,
+    required this.rental_price,
+    required this.description,
+    required this.imageUrl,
+    required this.createdAt,
+  });
+}
