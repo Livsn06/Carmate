4e389daa0c3c30900790a88f6d32b8a4ad00fac2
Jmgty <=>
2025-04-14 02:43:23 +0800

    for refreshing the data (must be change if possible soon)

--- lib/screens/car/sc-car-information.dart
+++ lib/screens/car/sc-car-information.dart
@@ -16,6 +16,9 @@ class CarInformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    carInformationController.onReady();
+
+    //
     size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
